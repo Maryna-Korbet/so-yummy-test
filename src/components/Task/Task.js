19637049b8fdf5bc64254ed5f28469c11d071492
Maryna-Korbet <m22v86k@gmail.com>
2023-01-25 23:44:15 +0200

    Deleting a task

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,11 +1,17 @@
 // Импортируем хук для отправки экшен
 import { useDispatch } from "react-redux";
+// Импортируем генератор экшена
+import { deleteTask } from "../../redux/actions";
 import { MdClose } from "react-icons/md";
 import css from "../Task/Task.module.css";
 
 export const Task = ({ task }) => {
   //Запускаем функцию отправки экшенов
   const dispatch = useDispatch();
+
+  // Вызываем генератор экшена и передаём идентификатор задачи
+  // Отправляем результат - экшен удаления задачи
+  const handleDelete = () => dispatch(deleteTask(task.id));
   
   return (
     <div className={css.wrapper}>
@@ -15,7 +21,7 @@ export const Task = ({ task }) => {
         checked={task.completed}
       />
       <p className={css.text}>{task.text}</p>
-      <button className={css.btn}>
+      <button className={css.btn} onClick={handleDelete}>
         <MdClose size={24} />
       </button>
     </div>
