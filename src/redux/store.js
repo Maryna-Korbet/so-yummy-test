67477dfa7dab75443a4467ca1f56058b4bc47f3c
Maryna-Korbet <m22v86k@gmail.com>
2023-01-25 21:57:21 +0200

    added enhancer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
 import { createStore } from "redux";
+import { devToolsEnhancer } from "@redux-devtools/extension";
 
-// Начальное значение состояния Redux для корневого редюсера,
-// если не передать параметр preloadedState.
 const initialState = {
-    task: [
+    tasks: [
         { id: 0, text: "Learn HTML and CSS", completed: true },
         { id: 1, text: "Get good at JavaScript", completed: true },
         { id: 2, text: "Master React", completed: false },
@@ -11,15 +10,15 @@ const initialState = {
         { id: 4, text: "Build amazing apps", completed: false },
     ],
     filters: {
-        status: 'all',
+        status: "all",
     },
 };
-
-// Пока что используем редюсер который
-// только возвращает полученное состояние
-const rootReducer = (state = initialState, action) => {
+    const rootReducer = (state = initialState, action) => {
     return state;
 };
 
-export const store = createStore(rootReducer);
+// Создаем расширение стора чтобы добавить инструменты разработчика
+const enhancer = devToolsEnhancer();
+
+export const store = createStore(rootReducer, enhancer);
 
