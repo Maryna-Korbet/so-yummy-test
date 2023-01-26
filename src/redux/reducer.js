830d5512dafd6672122168a0d570036076ac8ff5
Maryna-Korbet <m22v86k@gmail.com>
2023-01-26 02:26:50 +0200

    createReducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,6 @@
+import { createReducer } from "@reduxjs/toolkit";
 import { statusFilters } from "./constants";
-import { addTask, deleteTask, toggleCompleted } from "./actions";
+import { addTask, deleteTask, toggleCompleted, setStatusFilter } from "./actions";
 
 
 const tasksInitialState = [
@@ -10,42 +11,41 @@ const tasksInitialState = [
   { id: 4, text: "Build amazing apps", completed: false },
 ];
 
-export const tasksReducer = (state = tasksInitialState, action) => {
-  switch (action.type) {
+export const tasksReducer = createReducer(tasksInitialState, {
 
-    case addTask.type:
-      return [...state, action.payload];
+  [addTask]: (state, action) => {
+    return [...state, action.payload];
+  },
 
-    case deleteTask.type:
-      return state.filter(task => task.id !== action.payload);
-      
-    case toggleCompleted.type:
-      return state.map(task => {
-        if (task.id !== action.payload) {
-          return task;
-        }
-        return { ...task, completed: !task.completed };
-      });
+  [deleteTask]: (state, action) => {
+    return state.filter(task => task.id !== action.payload);
+  },
 
-    default:
-      return state;
-  }
-};
+  [toggleCompleted]: (state, action) => {
+    return state.map(task => {
+      if (task.id !== action.payload) {
+        return task;
+      }
+      return {
+        ...task,
+        completed: !task.completed,
+      };
+    });
+  },
+});
 
 const filtersInitialState = {
   status: statusFilters.all,
 };
 
-export const filtersReducer = (state = filtersInitialState, action) => {
-  switch (action.type) {
-    case "filters/setStatusFilter":
-      return {
+export const filtersReducer = createReducer(filtersInitialState, {
+  [setStatusFilter]: (state, action) => {
+    return {
         ...state,
         status: action.payload,
       };
-    default:
-      return state;
-  }
-};
+  },
+});
+
 
 
