f7cb444e51608be984d22f85f2432650c44703fb
Maryna-Korbet <m22v86k@gmail.com>
2023-01-28 11:45:59 +0200

    running a query behind a list of tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchTasks } from "redux/operations";
 import { Layout } from "components/Layout/Layout";
 import { AppBar } from "components/AppBar/AppBar";
 import { TaskForm } from "components/TaskForm/TaskForm";
 import { TaskList } from "components/TaskList/TaskList";
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+  
   return (
     <Layout>
       <AppBar />
