9e2297bdf4c53fb90be64c2132bf2b00188e238c
Maryna-Korbet <m22v86k@gmail.com>
2023-01-28 11:39:46 +0200

    start asynchronous operations, added operations

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,5 +1,4 @@
 import { useDispatch } from "react-redux";
-import { addTask } from "redux/tasksSlice";
 import { Button } from "components/Button/Button";
 import css from "./TaskForm.module.css";
 
@@ -9,7 +8,6 @@ export const TaskForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
-    dispatch(addTask(form.elements.text.value));
     form.reset();
   };
 
