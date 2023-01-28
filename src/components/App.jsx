2bae08c530540a8ac97b009a1b3f748da7aa8d66
Maryna-Korbet <m22v86k@gmail.com>
2023-01-28 19:06:34 +0200

    fixed the bug deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTasks } from "redux/operations";
-import { getError, getIsLoading } from "redux/selectors";
+import { getError, getIsLoading } from "redux/selector";
 import { Layout } from "components/Layout/Layout";
 import { AppBar } from "components/AppBar/AppBar";
 import { TaskForm } from "components/TaskForm/TaskForm";
