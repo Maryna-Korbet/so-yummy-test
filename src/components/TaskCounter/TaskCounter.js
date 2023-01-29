10c7ea1cfe91a4400c4aa3260acab935f91d0157
Maryna-Korbet <m22v86k@gmail.com>
2023-01-29 04:49:19 +0200

    made component selector selectTaskCount

--- src/components/TaskCounter/TaskCounter.js
+++ src/components/TaskCounter/TaskCounter.js
@@ -1,21 +1,9 @@
 import { useSelector } from "react-redux";
-import { selectTasks } from "redux/selector";
+import { selectTaskCount } from "redux/selector";
 import css from "./TaskCounter.module.css";
 
 export const TaskCounter = () => {
-  const tasks = useSelector(selectTasks);
-
-  const count = tasks.reduce(
-    (acc, task) => {
-      if (task.completed) {
-        acc.completed += 1;
-      } else {
-        acc.active += 1;
-      }
-      return acc;
-    },
-    { active: 0, completed: 0 }
-  );
+  const count = useSelector(selectTaskCount);
 
   return (
     <div>
@@ -24,3 +12,4 @@ export const TaskCounter = () => {
     </div>
   );
 };
+
